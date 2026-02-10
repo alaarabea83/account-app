c2083575209a82f787c105ab4d84a24a64e40b17
alaarabea83 <alaarabea83@gmail.com>
2026-02-10 04:07:23 +0200

    cashbalance

--- js/sales.js
+++ js/sales.js
@@ -74,7 +74,7 @@ function addInvoiceItem(product) {
   row.innerHTML = `
     <td>${rowNumber}</td>
     <td>${product.name}</td>
-    <td><input type="number" class="itemQty" placeholder="أدخل الكمية" min="1" value=""></td>
+    <td><input type="number" class="itemQty" placeholder="الكمية" min="1" value=""></td>
     <td><input type="number" class="itemPrice" value="${product.price}" readonly></td>
     <td><input type="number" class="itemTotal" value="${product.price}" readonly></td>
     <td><button type="button" class="btn-delete-item">❌</button></td>
